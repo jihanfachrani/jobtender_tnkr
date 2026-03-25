2a15cf592d1e3b1b89ad4551ab30d4169902bb3c
Jihan Fachrani <jihan.fachrani@unida.ac.id>
2026-03-25 14:46:22 +0700

    initial commit

--- js/script.js
+++ js/script.js
@@ -10,119 +10,29 @@ const data = await response.json()
 document.getElementById("judulTender").innerText = data.informasi_tender.judul
 document.getElementById("periodeTender").innerText = data.informasi_tender.periode
 
-
 /* =========================
    TAMPILKAN JABATAN
 ========================= */
 
 const jabatanTable = document.getElementById("jabatanTable")
 
-data.jabatan.forEach(j => {
+// data.peserta.sort((a, b) => a.jabatan_sasaran.localeCompare(b.jabatan_sasaran))
 
-jabatanTable.innerHTML += `
+data.peserta.forEach(p => {
 
-<tr>
-<td>${j.jabatan}</td>
-<td>${j.level}</td>
-</tr>
-
-`
-//<td>${j.lokasi}</td> (untuk manggil lokasi)
-
-})
-
-
-/* =========================
-   SELEKSI ADMINISTRATIF
-========================= */
-
-const adminTable = document.getElementById("adminTable")
-
-// Mandatory
-data.seleksi_administratif.mandatory.forEach(m => {
-
-adminTable.innerHTML += `
-
-<tr>
-
-<td>${m.rincian}</td>
-
-</tr>
-
-`
-{/* <td>${m.aspek}</td>
-<td><span class="badge mandatory">${m.metode}</span></td> */}
-
-})
-
-// // Ranking (3 aspek) dengan merge kolom metode + badge
-// const ranking = data.seleksi_administratif.ranking;
-// if (ranking.length > 0) {
-//   adminTable.innerHTML += `
-//     <tr>
-//       <td>${ranking[0].aspek}</td>
-//       <td>${ranking[0].rincian}</td>
-//       <td rowspan="${ranking.length}">
-//         <span class="badge ranking">Ranking</span>
-//       </td>
-//     </tr>
-//   `;
-//   for (let i = 1; i < ranking.length; i++) {
-//     adminTable.innerHTML += `
-//       <tr>
-//         <td>${ranking[i].aspek}</td>
-//         <td>${ranking[i].rincian}</td>
-//       </tr>
-//     `;
-//   }
-// }
-
-
-/* =========================
-   TAHAP WAWANCARA
-========================= */
-
-const wawancaraContainer = document.getElementById("wawancara")
-
-data.tahap_wawancara.forEach(w => {
-
-wawancaraContainer.innerHTML += `
-
-<div class="card">
-
-<h3>${w.aspek}</h3>
-
-<p>${w.rincian}</p>
-
-</div>
-
-`
-
-})
-
-
-/* =========================
-   JADWAL
-========================= */
-
-const jadwalTable = document.getElementById("jadwalTable")
-
-data.jadwal.forEach(j => {
-
-jadwalTable.innerHTML += `
-
-<tr>
-
-<td>${j.tahap}</td>
-<td>${j.tanggal}</td>
-<td>${j.waktu ? j.waktu : "-"}</td>
-
-</tr>
-
-`
+  jabatanTable.innerHTML += `
+  <tr>
+    <td>${p.nik}</td>
+    <td>${p.nama}</td>
+    <td>${p.jabatan_sasaran}</td>
+    <td>${p.level_sasaran}</td>
+  </tr>
+  `
 
 })
 
 }
 
 loadTender()
+
+
