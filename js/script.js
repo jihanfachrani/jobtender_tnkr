e5f53abf7e22e55397b4d4afc893cb3e3a00a2cc
Jihan Fachrani <jihan.fachrani@unida.ac.id>
2026-03-25 17:34:54 +0700

    Initial commit

--- js/script.js
+++ js/script.js
@@ -18,16 +18,32 @@ const jabatanTable = document.getElementById("jabatanTable")
 
 // data.peserta.sort((a, b) => a.jabatan_sasaran.localeCompare(b.jabatan_sasaran))
 
-data.peserta.forEach(p => {
+// data.jabatan.forEach((j, index) => {
 
-  jabatanTable.innerHTML += `
-  <tr>
-    <td>${p.nik}</td>
-    <td>${p.nama}</td>
-    <td>${p.jabatan_sasaran}</td>
-    <td>${p.level_sasaran}</td>
-  </tr>
-  `
+// jabatanTable.innerHTML += `
+// <tr>
+// <td>${index + 1}</td> 
+// <td>${j.nik}</td>
+// <td>${j.nama}</td>
+// <td>${j.jabatan}</td>
+// <td>${j.level_sasaran}</td>
+// </tr>
+// `
+
+// })
+
+data.peserta.forEach((j, index) => {
+
+jabatanTable.innerHTML += `
+<tr>
+<td>${index + 1}</td> 
+<td>${j.nik}</td>
+<td>${j.nama}</td>
+<td>${j.jabatan_sasaran}</td>
+<td>${j.level_sasaran}</td>
+<td>${j.lokasi}</td>
+</tr>
+`
 
 })
 
